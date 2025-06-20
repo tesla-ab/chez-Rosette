71d2add2dfef111995359aa8a06e6bf10415421a
Abdelaaziz.bahssou <azizbahssou444@gmail.com>
2025-06-20 22:02:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-const images = [
-  'jr-r-90HdOlGbjck-unsplash.jpg',
-  'oliver-timm-haarmann-p6OLZPnq810-unsplash.jpg',
-  'resized_brooke-lark-V4MBq8kue3U-unsplash.jpg',
-  'resized_heather-barnes-WbZesfqwR-A-unsplash.jpg',
-  'sebastian-coman-photography-dacM7egRw0Y-unsplash.jpg',
-];
-
+const images = document.querySelectorAll('.bg-image');
 let index = 0;
-const mainSection = document.querySelector('.main');
 
 setInterval(() => {
+  images[index].classList.remove('active');
   index = (index + 1) % images.length;
-  mainSection.style.backgroundImage = `url('${images[index]}')`;
-  mainSection.style.backgroundPosition = 'right';
-  mainSection.style.backgroundSize = 'contain'; // باش تكون الصورة مناسبة للمساحة
-}, 5000);
+  images[index].classList.add('active');
+}, 6000);
